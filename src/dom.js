df02be3c8a2f5233725f3c4a1f7ae4b4effe817a
Marty <maruri2424@gmail.com>
2022-04-13 23:07:02 -0700

    wrote loadMenu

--- src/dom.js
+++ src/dom.js
@@ -18,8 +18,13 @@ function pageLoad() {
   navDiv.appendChild(navInfo);
   nav.appendChild(navDiv);
   content.appendChild(nav);
-  const main = document.createElement('div');
-  main.id = 'main';
+  loadMenu();
+}
+
+function loadHome() {
+  const content = document.getElementById('content');
+  const home = document.createElement('div');
+  home.id = 'home';
   const headline = document.createElement('div');
   headline.id = 'headline';
   const h1 = document.createElement('h1');
@@ -30,9 +35,47 @@ function pageLoad() {
   const p = document.createElement('p');
   p.textContent = '署づ再1出聞ヲヌヱレ別続ドだ村表保団べふ介51護おド公北ヲヤムネ週住限ふたぴし所宅クだ。際ナヨキワ牧登ワ領収済フや代要エ玲下セホソ経害ぎづくら装本曽ウ上月さえぱけ大13見話ケ盛田ミケノテ的邪ヱフノチ家子サユチソ委練僚弱かえぽ。止よさほも歌不ざへほう球8要タム真毎りレはン幅報ごあフげ豪日ナコホ医96上ぶろゅび城提ヨエフ事際にわい複込ツレメ記夕ぶン題供米ふ供省ご井異技のんトス必剛させだ。';
   copy.appendChild(p);
-  main.appendChild(headline);
-  main.appendChild(copy);
-  content.appendChild(main);
+  home.appendChild(headline);
+  home.appendChild(copy);
+  content.appendChild(home);
+}
+
+function loadMenu() {
+  const content = document.getElementById('content');
+  const menu = document.createElement('div');
+  menu.id = 'menu';
+  const head = document.createElement('div');
+  const headH1 = document.createElement('h1');
+  headH1.textContent = 'Menu';
+  head.id = 'menuHead';
+  head.appendChild(headH1);
+  const i1 = document.createElement('div');
+  i1.classList.add('menuItem');
+  const i1H2 = document.createElement('h2');
+  i1H2.textContent = 'Sushi Set A';
+  const i1p = document.createElement('p');
+  i1p.textContent = '新ーゃすぞ念地リ月祭コ柏力どスろ及両ッ問選ひ両見べのほト';
+  i1.appendChild(i1H2);
+  i1.appendChild(i1p);
+  const i2 = document.createElement('div');
+  const i2H2 = document.createElement('h2');
+  i2H2.textContent = 'Sushi Set B';
+  const i2p = document.createElement('p');
+  i2p.textContent = '新ーゃすぞ念地リ月祭コ柏力どスろ及両ッ問選ひ両見べのほト';
+  i2.appendChild(i2H2);
+  i2.appendChild(i2p);
+  menu.appendChild(head);
+  menu.appendChild(i1);
+  menu.appendChild(i2);
+  content.appendChild(menu);
+}
+
+function loadInfo() {
+
+}
+
+function clear() {
+
 }
 
 export default pageLoad;
